899a3e195b3cf671b11a9da2a1eef2f4baef5116
Caio Lima <107970067+caiolimasantos@users.noreply.github.com>
2024-02-13 02:40:20 -0300

    create new feature

--- app.js
+++ app.js
@@ -11,9 +11,14 @@ function changeTheme(){
         if(document.querySelector('.fa-solid').classList[1] == 'fa-sun'){
             document.querySelector('.fa-solid').classList.replace('fa-sun','fa-moon');
             document.querySelector('body').classList.replace('light-mode','dark-mode');
+            document.querySelector('.tittle').classList.replace('light-mode','dark-mode');
+            document.querySelector('.sub-tittle').classList.replace('light-mode','dark-mode');
+           
         } else {
             document.querySelector('.fa-solid').classList.replace('fa-moon','fa-sun');
             document.querySelector('body').classList.replace('dark-mode','light-mode');
+            document.querySelector('.tittle').classList.replace('dark-mode','light-mode');
+            document.querySelector('.sub-tittle').classList.replace('dark-mode','light-mode');
         }
 
         
