b7ca85c8b1140b70ad1063044ecd22f7f2e58051
Caio Lima <107970067+caiolimasantos@users.noreply.github.com>
2024-02-13 13:11:44 -0300

    create page contao

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ window.addEventListener('load', ()=>{
     preloader.classList.add('preloader-finish');
     changeTheme();
     scrollToTop();
-    window.scrollTo(0,0);
 })
 window.addEventListener('scroll',()=>{
     if(window.scrollY >= 10){
