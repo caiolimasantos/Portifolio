1db41a715c2c817052c400b19701067f735eddf3
Caio Lima <107970067+caiolimasantos@users.noreply.github.com>
2024-02-13 00:44:27 -0300

    create darkmode

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 window.addEventListener('load', ()=>{
     const preloader = document.querySelector('.preloader');
     preloader.classList.add('preloader-finish');
- 
+    changeTheme();
 })
+
+function changeTheme(){
+    const buttonChangetheme = document.getElementById('changetheme');
+
+    buttonChangetheme.addEventListener('click', ()=>{
+        if(document.querySelector('.fa-solid').classList[1] == 'fa-sun'){
+            document.querySelector('.fa-solid').classList.replace('fa-sun','fa-moon');
+            document.querySelector('body').classList.replace('light-mode','dark-mode');
+        } else {
+            document.querySelector('.fa-solid').classList.replace('fa-moon','fa-sun');
+            document.querySelector('body').classList.replace('dark-mode','light-mode');
+        }
+
+        
+    })
+}
